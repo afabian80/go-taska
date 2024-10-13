7b11ee5d0b2ad6f2b9e416a24ddc45268cd21447
Akos Fabian <fabian.akos@gmail.com>
2024-10-13 19:57:03 +0200

    move model methods

--- main.go
+++ main.go
@@ -14,33 +14,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case tea.KeyMsg:
-		m.timetick++
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up":
-			m.taskList.moveUp()
-		case "down":
-			m.taskList.moveDown()
-		case "a":
-			m.taskList.addDefault()
-		case "delete":
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	result := fmt.Sprintf("Timetick: %d\n", m.timetick)
-	result += fmt.Sprintf("TaskList: %v\n", m.taskList)
-	return result
-}
--- model.go
+++ model.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
 type model struct {
 	timetick int
 	taskList TaskList
@@ -10,4 +16,34 @@ func initialModel() model {
 		timetick: 0,
 		taskList: NewTaskList(),
 	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		m.timetick++
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up":
+			m.taskList.moveUp()
+		case "down":
+			m.taskList.moveDown()
+		case "a":
+			m.taskList.addDefault()
+		case "delete":
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	result := fmt.Sprintf("Timetick: %d\n", m.timetick)
+	result += fmt.Sprintf("TaskList: %v\n", m.taskList)
+	return result
 }
